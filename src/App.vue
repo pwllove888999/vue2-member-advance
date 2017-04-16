d5c7563b28932f3a90cd9102b4750efa63d36cd5
Pengwei <492941566@qq.com>
2017-04-17 00:47:50 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <div class="page-wrapper">
+      <v-header></v-header>
+      <div class="page-content">
+        <v-menu></v-menu>
+        <v-main></v-main>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Header from './components/header/header.vue'
+import Menu from './components/menu/menu.vue'
+import Main from './components/main/main.vue'
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    'v-header': Header,
+    'v-menu': Menu,
+    'v-main': Main,
+  }
+}
+</script>
+
+<style>
+/*#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}*/
+</style>
---
+++ src/components/menu/menu.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <div class="menu">
+    这里是菜单栏
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
